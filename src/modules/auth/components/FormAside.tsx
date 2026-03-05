c6fd140755ec10a81d0e95db4653740849941914
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-05 17:44:36 +0400

    fix: form aside scroll

--- src/modules/auth/components/FormAside.tsx
+++ src/modules/auth/components/FormAside.tsx
@@ -2,21 +2,23 @@ import Image from "next/image";
 
 export default function FormAside() {
   return (
-    <div className="relative max-h-270 h-fit bg-(--neutral-semi-white) flex items-center justify-center">
+    <div className="relative max-h-screen h-fit bg-(--neutral-semi-white) flex items-center justify-center">
       <Image
         src="/logo.png"
         alt="Company logo"
         width={105}
         height={24}
-        className="absolute left-1/2 -translate-x-1/2 top-13"
+        className="absolute left-1/2 -translate-x-1/2 top-13 z-1"
       />
 
-      <Image
-        src="/sofa.png"
-        alt="Sofa placeholder photo"
-        width={736}
-        height={1080}
-      />
+      <div className="relative w-full h-screen">
+        <Image
+          src="/sofa.png"
+          alt="Sofa placeholder photo"
+          fill
+          className="object-cover"
+        />
+      </div>
     </div>
   );
 }
