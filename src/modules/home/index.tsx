bbcba8fc4fbd94a63cb6909ce87ce027ad577521
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-10 02:01:45 +0400

    feat: homepage components shown in layout

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,11 @@
+import CategoryCardsLayout from "../categories/components/sections/CategoryCardsLayout";
+import { Slider } from "./components/sections/Slider";
+
+export default function HomePage() {
+  return (
+    <>
+      <Slider />
+      <CategoryCardsLayout />
+    </>
+  );
+}
