ecba61a399a8a4695cb023288530825ae3ba8074
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-09 17:44:00 +0400

    fix: auth and middleware

--- src/auth/helpers.ts
+++ src/auth/helpers.ts
@@ -1,20 +1,27 @@
 import { user_role } from "@/generated/prisma/enums";
 import { validateRequest } from "./validate";
+import { HTTPException } from "hono/http-exception";
 
 export const requireAuth = async () => {
   const { user, session } = await validateRequest();
 
-  if (!user || !session) throw new Error("Unauthorized");
+  if (!user || !session) {
+    throw new HTTPException(401, {
+      message: "ნგარიშში შესვლის გარეშე ამ ფუნქციას ვერ გამოიყენებ!",
+    });
+  }
 
   return { user, session };
 };
 
-export const requireRole = async (role: user_role[]) => {
-  const { user, session } = await validateRequest();
+export const requireRole = async (roles: user_role[]) => {
+  const { user, session } = await requireAuth();
 
-  if (!user || !session) throw new Error("Unauthorized");
-
-  if (!role.includes(user.role)) throw new Error("Unauthorized");
+  if (!roles.includes(user.role)) {
+    throw new HTTPException(403, {
+      message: "არ გაქვს უფლება ამ ფუნქციის გამოყენების!",
+    });
+  }
 
   return { user, session };
 };
