1154de9e61dd0743a06dc0b262b2ec14051c754f
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-29 19:18:05 +0400

    feat: responsive layout

--- src/app/(shop)/(dashboard)/layout.tsx
+++ src/app/(shop)/(dashboard)/layout.tsx
@@ -10,11 +10,11 @@ export default async function dashboardLayout({
 
   if (!user || !session) redirect("/login");
   return (
-    <div className="flex items-center flex-col max-w-280 mx-auto w-full pb-10">
+    <div className="flex items-center flex-col max-w-280 mx-auto w-full pb-10 px-8 w1120:px-0">
       <h1 className="py-20 leading-11 text-[40px] sm:leading-14.5 sm:text-[54px] font-medium">
         My Account
       </h1>
-      <main className="flex gap-10 items-start w-full">
+      <main className="flex gap-10 items-center w-full flex-col sm:items-start sm:flex-row">
         <UserAside />
         {children}
       </main>
