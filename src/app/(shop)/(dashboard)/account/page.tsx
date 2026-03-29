5ca86e8c5b6342c7df1384eb281fdf3001da6060
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2026-03-29 16:46:32 +0400

    feat: dashboads route in app folder

---
+++ src/app/(shop)/(dashboard)/account/page.tsx
@@ -0,0 +1,5 @@
+import AccountPage from "@/src/modules/user-dashboard/components/sections/AccountPage";
+
+export default function page() {
+  return <AccountPage />;
+}
---
+++ src/app/(shop)/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { requireAuth } from "@/src/auth/helpers";
+import { UserAside } from "@/src/modules/user-dashboard/components/sections/UserAside";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function dashboardLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  const { user, session } = await requireAuth();
+
+  if (!user || !session) redirect("/login");
+  return (
+    <div className="flex items-center flex-col max-w-280 mx-auto w-full pb-10">
+      <h1 className="py-20 leading-11 text-[40px] sm:leading-14.5 sm:text-[54px] font-medium">
+        My Account
+      </h1>
+      <main className="flex gap-10 items-start w-full">
+        <UserAside />
+        {children}
+      </main>
+    </div>
+  );
+}
